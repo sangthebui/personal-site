e38502aa2d4ad9c0568a8caaa0e86f0d09629a29
Sang Bui <sangbui51@yahoo.com>
2022-08-04 15:55:27 +0700

    Fix a bug in Intro with name.

--- src/Intro.js
+++ src/Intro.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Jumbotron } from 'reactstrap';
 
-const Intro = () => (
+const Intro = ({name}) => (
     <>
         <section className="introBackground" />
         <Jumbotron >
-            <h2 style={{color: "#22A39F", fontSize: "100px"}}>Sang Bui</h2>
+            <h2 style={{color: "#22A39F", fontSize: "100px"}}>{name}</h2>
         </Jumbotron>
     </>
 );
